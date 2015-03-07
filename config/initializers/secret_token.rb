10ca98c8dc2ebe744cd308bfcb058ba109816a20
yuki <yuki@grappy.jp>
2015-03-07 16:29:04 +0900

    first git adding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ee6dff9a7e566980c7baaffe4edf03a8bcad33d3b5b1a17ce156f4acc79a6ad475d36f50ba596838248ac151b681a936130416d40b68a5d4c71c5cabfc565171'
